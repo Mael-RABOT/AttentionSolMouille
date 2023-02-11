1f29982a42abdd7b558e5fa3c54b72483215f632
Yohann <102670831+YohannMgt@users.noreply.github.com>
2023-02-11 10:20:21 +0100

    Update microphone.py

--- microphone.py
+++ microphone.py
@@ -11,16 +11,16 @@ WAVE_OUTPUT_FILENAME = "file.wav"
 TIME_BETWEEN_RECORDS = 5
 NB_FILES = 5
 
-def record_audio(wave_output_filename=WAVE_OUTPUT_FILENAME, nb=0):
+def record_audio(wave_output_filename=WAVE_OUTPUT_FILENAME, nb=0, nb_files=0):
     audio = pyaudio.PyAudio()
     stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
-    print(f"[{nb}] recording...")
+    print(f"[{nb}/{nb_files}] recording...")
     frames = []
 
     for _ in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
         data = stream.read(CHUNK)
         frames.append(data)
-    print(f"[{nb}] finished recording")
+    print(f"[{nb}/{nb_files}] finished recording")
 
     stream.stop_stream()
     stream.close()
@@ -33,7 +33,12 @@ def record_audio(wave_output_filename=WAVE_OUTPUT_FILENAME, nb=0):
     wave_file.writeframes(b''.join(frames))
     wave_file.close()
 
-for i in range(NB_FILES):
-    pass
-    #record_audio("file" + str(i + 1) + ".wav", i + 1)
-    #time.sleep(TIME_BETWEEN_RECORDS)
+def start_record(nb_files):
+    for i in range(nb_files):
+        record_audio("file" + str(i + 1) + ".wav", i + 1, NB_FILES)
+        if i < NB_FILES - 1:
+            print("wait...")
+            time.sleep(TIME_BETWEEN_RECORDS)
+    print("Done!")
+
+#start_record(NB_FILES)
